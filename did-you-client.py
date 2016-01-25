8235bbc92bfe5129e1369cd55b8900a3686f917f
femnad <zehemer@fastmail.fm>
2016-01-25 23:48:45 +0200

    Use gevent for Spawning Req and Rep

--- did-you-client.py
+++ did-you-client.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python2
+import gevent
 import sys
-import zmq
+import zmq.green as zmq
 from did_you_pb2 import TaskCommand, Task, TaskList
 
-context = zmq.Context()
+class TaskCommander(object):
 
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+    def __init__(self):
+        context = zmq.Context()
+        self._socket = context.socket(zmq.REQ)
+        self._socket.connect("tcp://localhost:5555")
 
-if len(sys.argv) < 2:
-    executable_name = sys.argv[0]
-    print("Usage: {} <command> [<task>]".format(executable_name))
-    exit()
-else:
-    command = sys.argv[1]
-    if command.upper() not in TaskCommand.Command.keys():
-        print("Unknown command: {}".format(command))
-        exit()
-    task_command = TaskCommand()
-    task_command.command = TaskCommand.Command.Value(command.upper())
-    if len(sys.argv) == 3:
-        task_name = sys.argv[2]
+    def run_command(self, command, task_name):
+        if command.upper() not in TaskCommand.Command.keys():
+            return
+        task_command = TaskCommand()
+        task_command.command = TaskCommand.Command.Value(command.upper())
         task_command.task.name = task_name
-    socket.send(task_command.SerializeToString())
-    message = socket.recv()
-    if len(sys.argv) == 2 and command.upper() == 'LIST':
-        task_list = TaskList.FromString(message)
-        for task in task_list.tasks:
-            print(task.name)
-    else:
-        print(message)
+        self._socket.send(task_command.SerializeToString())
+        message = self._socket.recv()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        executable_name = sys.argv[0]
+        print("Usage {} <command> <task-name>".format(executable_name))
+        exit()
+    command, task_name = sys.argv[1:]
+    task_commander = TaskCommander()
+    request = gevent.spawn(task_commander.run_command, command, task_name)
+    gevent.joinall([request])
--- did-you-server.py
+++ did-you-server.py
@@ -1,38 +1,49 @@
 #!/usr/bin/env python2
-import zmq
+import gevent
+import zmq.green as zmq
 from did_you_pb2 import TaskCommand, Task, TaskList
 
-def print_tasks(task_list):
-    if len(task_list) == 0:
-        print('No tasks')
-    else:
-        print('Incomplete Tasks')
-        for item_no, task in enumerate(task_list, 1):
-            print("{}: {}".format(item_no, task))
+class TaskServer(object):
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
-tasks = set()
+    def __init__(self):
+        self._tasks = set()
 
-while True:
-    message = socket.recv()
-    task_command = TaskCommand()
-    task_command = task_command.FromString(message)
-    if TaskCommand.Command.Name(task_command.command) == 'DO':
-        tasks.add(task_command.task.name)
-        socket.send(b"Ack")
-    elif TaskCommand.Command.Name(task_command.command) == 'DONE':
-        task = task_command.task
-        if task.name in tasks:
-            tasks.remove(task_command.task.name)
-        socket.send(b"Ack")
-    elif TaskCommand.Command.Name(task_command.command) == 'LIST':
-        task_list = TaskList()
-        for task_name in tasks:
-            task = task_list.tasks.add()
-            task.name = task_name
-        socket.send(task_list.SerializeToString())
-    else:
-        print("Unknown command:")
-    print_tasks(tasks)
+    def print_tasks(self):
+        if len(self._tasks) == 0:
+            print('No tasks')
+        else:
+            print('Incomplete Tasks')
+            for item_no, task in enumerate(self._tasks, 1):
+                print("{}: {}".format(item_no, task))
+
+    def serve(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind("tcp://*:5555")
+
+        while True:
+            message = socket.recv()
+            task_command = TaskCommand()
+            task_command = task_command.FromString(message)
+            if TaskCommand.Command.Name(task_command.command) == 'DO':
+                self._tasks.add(task_command.task.name)
+                socket.send(b"Ack")
+            elif TaskCommand.Command.Name(task_command.command) == 'DONE':
+                task = task_command.task
+                if task.name in self._tasks:
+                    self._tasks.remove(task_command.task.name)
+                socket.send(b"Ack")
+            elif TaskCommand.Command.Name(task_command.command) == 'LIST':
+                task_list = TaskList()
+                for task_name in self._tasks:
+                    task = task_list.tasks.add()
+                    task.name = task_name
+                socket.send(task_list.SerializeToString())
+            else:
+                print("Unknown command:")
+            self.print_tasks()
+
+if __name__ == "__main__":
+    task_server = TaskServer()
+    task_serving = gevent.spawn(task_server.serve)
+    gevent.joinall([task_serving])
