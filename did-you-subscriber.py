1b60834aef74b78ac680228035a6928740dfb618
femnad <zehemer@fastmail.fm>
2016-01-26 00:00:59 +0200

    Subscribe on Everything

--- did-you-subscriber.py
+++ did-you-subscriber.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 import gevent
 import zmq.green as zmq
+from did_you_pb2 import TaskList
 
 class TaskSubscriber(object):
 
@@ -8,10 +9,14 @@ class TaskSubscriber(object):
         context = zmq.Context()
         self._socket = context.socket(zmq.SUB)
         self._socket.connect("tcp://localhost:5556")
+        self._socket.setsockopt(zmq.SUBSCRIBE, b'')
 
     def get_task_list(self):
         message = self._socket.recv()
-        print(message)
+        task_list = TaskList()
+        task_list = task_list.FromString(message)
+        for task in task_list.tasks:
+            print(task.name)
 
 if __name__ == "__main__":
     task_subscriber = TaskSubscriber()
