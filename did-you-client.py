a74ba0becf77a004706c512d5616c0f910a82db0
femnad <zehemer@fastmail.fm>
2016-02-06 18:10:27 +0200

    Use Enum For Represting Task Commands

--- did-you-client.py
+++ did-you-client.py
@@ -4,6 +4,7 @@ import sys
 import zmq
 
 from did_you_config import DidYouConfig
+from task_command import TaskCommand
 
 
 class TaskCommander(object):
@@ -17,20 +18,41 @@ class TaskCommander(object):
         self._socket.connect("tcp://{}:{}".format(host, port))
 
     def run_command(self, command, task_name):
-        task_message = {"name": task_name, "command": command}
+        task_message = {"name": task_name, "command": command.value}
         self._socket.send(msgpack.packb(task_message))
         return self._socket.recv()
 
+class TaskSubscriber(object):
+
+    def __init__(self):
+        configurator = DidYouConfig()
+        host = configurator.host
+        port = configurator.subscription_port
+        context = zmq.Context()
+        self._socket = context.socket(zmq.SUB)
+        self._socket.connect("tcp://{}:{}".format(host, port))
+        self._socket.setsockopt(zmq.SUBSCRIBE, b'')
+
+    def get_task_list(self):
+        task_list = msgpack.unpackb(self._socket.recv())
+        for task in task_list:
+            print(task)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if not 2 <= len(sys.argv) <= 3:
         executable_name = sys.argv[0]
-        print("Usage {} <command> <task-name>".format(executable_name))
+        print("Usage {} <command> [<task-name>]".format(executable_name))
         exit()
-    command, task_name = sys.argv[1:]
-    task_commander = TaskCommander()
-    try:
-        response = task_commander.run_command(command, task_name)
-        print("Got response: {}".format(str(response, 'UTF-8')))
-    except AttributeError:
-        print("No such command: {}".format(command))
+    command = sys.argv[1]
+    if command == 'list':
+        task_subscriber = TaskSubscriber()
+        task_subscriber.get_task_list()
+    else:
+        task_name = sys.argv[2]
+        task_commander = TaskCommander()
+        try:
+            response = task_commander.run_command(
+                TaskCommand[command], task_name)
+            print("Got response: {}".format(str(response, 'UTF-8')))
+        except AttributeError:
+            print("No such command: {}".format(command))
---
+++ task_command.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class TaskCommand(Enum):
+    do = 1
+    done = 2
