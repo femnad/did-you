ab213ad761f272cf9d3ea6ad522e9c79cd125baf
femnad <zehemer@fastmail.fm>
2016-02-13 15:16:25 +0200

    Copy the Sample Config To /usr/local/etc

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setuptools.setup(
     name='did_you',
     packages=setuptools.find_packages(),
     include_package_data=True,
+    data_files = [
+        ('/config/did_you.conf.sample'), ('/usr/local/etc/did_you.conf')],
     entry_points={'console_scripts':
                   ['did_you_server = did_you.command:run_server']},
     )
