15928a08c1393b92833ec158369b8fa82e1c38ec
femnad <zehemer@fastmail.fm>
2016-02-13 15:54:59 +0200

    Switch to Pbr

--- setup.py
+++ setup.py
@@ -2,11 +2,5 @@
 import setuptools
 
 setuptools.setup(
-    name='did_you',
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    data_files = [
-        ('/usr/local/etc', ['config/did_you.conf'])],
-    entry_points={'console_scripts':
-                  ['did_you_server = did_you.command:run_server']},
-    )
+    setup_requires=['pbr>=1.8'],
+    pbr=True)
