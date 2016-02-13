bfb078d1a0ed5be190d7b46c6fe030dca72fab94
femnad <zehemer@fastmail.fm>
2016-02-13 16:08:44 +0200

    Parse Cmdargs in a Non-DRY Manner

--- did_you/command/__init__.py
+++ did_you/command/__init__.py
@@ -1,17 +1,19 @@
 # Package: did_you_server.command
 import os.path
+import sys
 import threading
 
 from did_you.config import DidYouConfig
-from did_you.utils import cmdline_args
 from did_you.server import TaskServer
 
 DEFAULT_CONFIG_FILE = "~/.did_you.conf"
 
-@cmdline_args
-def run_server(config_file):
-    if config_file is None:
+def run_server():
+    arguments = sys.argv[1:]
+    if len(arguments) == 0:
         config_file = DEFAULT_CONFIG_FILE
+    else:
+        config_file = arguments[0]
     if config_file.startswith('~'):
         config_file = os.path.expanduser(config_file)
     configurator = DidYouConfig(config_file)
--- did_you/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def cmdline_args(decoratee):
-    arguments = sys.argv[1:]
-    if len(arguments) == 0:
-        decoratee(None)
-    else:
-        decoratee(*arguments)
